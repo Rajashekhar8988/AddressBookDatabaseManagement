ccb4ba44c8140a8a1253b74e0fa8a716ac3cc37f
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-10-09 22:22:16 +0530

    Modified : to add contacts address book

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -5,3 +5,6 @@ use Address_Book_Service;
 
 -- Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes
 create table Address_Book(First_Name  varchar(150),Last_Name varchar(150),Address varchar(150),City varchar(150),PhoneNumber char(15),Zipcode char(10),Email varchar(255));
+
+-- Ability to insert new Contacts to Address Book
+insert into Address_Book(First_Name,Last_Name,Address,City,PhoneNumber,Zipcode,Email) values ('Rajashekhar','Reddy','Nagaram','Hyderabad','9876543214','500064','rajashekharraj3@gmail.com'),('Nani','Reddy','Ecil','Hyderabad','9876498741','783743','nanireddy2@gmail.com');
