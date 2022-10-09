2b17fbc5785270a2616fd3fd0874f862cde60716
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-10-09 22:20:00 +0530

    Modified : to create table address book and add columns

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -2,3 +2,6 @@
 -- to use the database
 create database Address_Book_Service;
 use Address_Book_Service;
+
+-- Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes
+create table Address_Book(First_Name  varchar(150),Last_Name varchar(150),Address varchar(150),City varchar(150),PhoneNumber char(15),Zipcode char(10),Email varchar(255));
